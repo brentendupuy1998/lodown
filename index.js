de62d5ed18310b23920593f6aaf96d1edd58909d
Brenten Dupuy <brentendupuy1998@gmail.com>
2017-11-29 21:53:49 +0000

    Added more detail to Lodown

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ function unique(array) {
  * Then return the new array
  * @param {array or object} collection, that needs to loop over
  * @param {function}
- * @return {array} with new unique elements
+ * @return {array} an array having each element from array parameter, then changed by function parmeter
  */
 
 
